b99f9e94f3b90ab56e908cfc0efb96ef8a5c17f9
Dheshan Mohandass <dmohandass@mgh.harvard.edu>
2025-07-08 18:00:19 +0000

    chore: Update documentation in compute_converted.py for clarity and consistency

--- formsdb/runners/compute/compute_converted.py
+++ formsdb/runners/compute/compute_converted.py
@@ -23,13 +23,13 @@ except ValueError:
 import logging
 import warnings
 from datetime import datetime
-from typing import Any, Dict, List, Optional
+from typing import Any, Dict, Optional
 
 import numpy as np
 import pandas as pd
 from rich.logging import RichHandler
 
-from formsdb import constants, data
+from formsdb import data
 from formsdb.helpers import db, utils
 
 MODULE_NAME = "formsdb.runners.compute.compute_converted"
@@ -56,7 +56,9 @@ def count_converted(config_file: Path) -> int:
     Returns:
         int: Number of subjects that have been converted.
     """
-    query = "SELECT COUNT(*) FROM forms_derived.conversion_status WHERE converted = 'True';"
+    query = (
+        "SELECT COUNT(*) FROM forms_derived.conversion_status WHERE converted = 'True';"
+    )
     converted_count_r = db.fetch_record(config_file=config_file, query=query)
     if converted_count_r is None:
         raise ValueError("No converted subjects found in the database.")
@@ -139,111 +141,7 @@ def get_conversion_date(subject_id: str, config_file: Path) -> Optional[datetime
     return None
 
 
-def get_converted_date_rpms(subject_id: str, config_file: Path) -> Optional[datetime]:
-    """
-    Checks subject's conversion forms for conversion status.
-
-    Specific to subjects that use RPMS
-
-    Args:
-        subject_id (str): The subject ID.
-        config_file (Path): Path to the config file.
-
-    Returns:
-        bool: Whether the subject has been converted.
-    """
-    query = f"""
-        SELECT * FROM forms.rpms_client_status WHERE subject_id = '{subject_id}'
-    """
-
-    df = db.execute_sql(config_file=config_file, query=query)
-
-    if df.empty:
-        return None
-
-    if df["Transition / Conversion"][0] is not None:
-        conversion_date = df["Transition / Conversion"][0]
-        conversion_date_dt = datetime.strptime(
-            conversion_date, "%d/%m/%Y"
-        )  # Updated format string
-        return conversion_date_dt
-
-    return None
-
-
-def get_converted_visit_psychs(
-    df: pd.DataFrame, visit_order: List[str], debug: bool = False
-) -> Optional[str]:
-    """
-    Check if a subject has been converted.
-
-    Uses the psychs_p9ac32_fu form to determine if a subject has been converted.
-
-    Args:
-        df (pd.DataFrame): DataFrame containing the forms data.
-        visit_order (List[str]): List of visit names in the order they were collected.
-        debug (bool, optional): Whether to print debug messages. Defaults to False.
-
-    Returns:
-        Optional[str]: The visit at which the subject converted, if any.
-    """
-    psychs_df = df[df["form_name"].str.contains("psychs_p9ac32_fu")]
-
-    if psychs_df.shape[0] == 0:
-        return None
-
-    for visit in visit_order:
-        visit_psychs_df = psychs_df[psychs_df["event_name"].str.contains(f"{visit}_")]
-        if visit_psychs_df.shape[0] == 0:
-            if debug:
-                print(f"No psychs_p9ac32_fu form found for {visit}.")
-            continue
-
-        form_r = visit_psychs_df.iloc[0, :]
-        form_data: Dict[str, Any] = form_r["form_data"]
-
-        form_variables = list(form_data.keys())
-
-        variable = "chrpsychs_fu_ac1_conv"
-        if variable in form_variables:
-            if form_data[variable] == 1:
-                if debug:
-                    print(f"Subject converted at {visit}.")
-                return visit
-        else:
-            if debug:
-                print(f"{variable} not found in {visit}.")
-
-    return None
-
-
-def get_converted_visit_rpms(subject_id: str, config_file: Path) -> Optional[str]:
-    """
-    Check's ClientStatus table for the converted visit for RPMS subjects.
-
-    Args:
-        subject_id (str): The subject ID.
-        config_file (Path): Path to the config file.
-
-    Returns:
-        str: The converted visit.
-    """
-
-    converted_date = get_converted_date_rpms(
-        subject_id=subject_id, config_file=config_file
-    )
-    if converted_date is None:
-        return None
-    closest_timepoint = data.get_closest_timepoint(
-        subject_id=subject_id, date=converted_date, config_file=config_file
-    )
-
-    return closest_timepoint
-
-
-def compute_converted(
-    config_file: Path, visit_order: List[str], debug: bool = False
-) -> pd.DataFrame:
+def compute_converted(config_file: Path) -> pd.DataFrame:
     """
     Check if subjects have been converted.
 
@@ -282,60 +180,33 @@ def compute_converted(
 
                 logger.info(f"Subject {subject_id} is overidden to be converted.")
 
-            df = data.get_all_subject_forms(
-                config_file=config_file, subject_id=subject_id
-            )
             converted = check_if_converted_conversion_form(
                 subject_id=subject_id, config_file=config_file
             )
             if converted:
                 conversion_info_source = "conversion_form"
 
-            converted_visit_psychs = get_converted_visit_psychs(
-                df=df, visit_order=visit_order, debug=debug
+            conversion_date = get_conversion_date(
+                subject_id=subject_id, config_file=config_file
             )
-            if converted_visit_psychs is None:
-                converted_visit = np.nan
-            else:
-                if conversion_info_source is None:
-                    conversion_info_source = "psychs_p9ac32_fu"
-                else:
-                    conversion_info_source += ", psychs_p9ac32_fu"
-
-            if data.subject_uses_rpms(subject_id=subject_id, config_file=config_file):
-                converted_visit_rpms = get_converted_visit_rpms(
-                    subject_id=subject_id, config_file=config_file
+            if conversion_date is not None and (
+                converted_visit is np.nan or converted_visit is None
+            ):
+                converted_visit = data.get_closest_timepoint(
+                    subject_id=subject_id,
+                    date=conversion_date,
+                    config_file=config_file,
                 )
-                if converted_visit_rpms is not None:
-                    converted_visit = converted_visit_rpms
-                    converted = True
-                    if conversion_info_source is None:
-                        conversion_info_source = "client_status"
-                    else:
-                        conversion_info_source += ", client_status"
-                conversion_date = get_converted_date_rpms(
-                    subject_id=subject_id, config_file=config_file
+            if conversion_date is not None and "conversion_form" not in str(
+                conversion_info_source
+            ):
+                conversion_info_source = (
+                    str(conversion_info_source) + ", conversion_form"
                 )
-            else:
-                conversion_date = get_conversion_date(
-                    subject_id=subject_id, config_file=config_file
-                )
-                if conversion_date is not None and converted_visit is np.nan:
-                    converted_visit = data.get_closest_timepoint(
-                        subject_id=subject_id,
-                        date=conversion_date,
-                        config_file=config_file,
-                    )
-                if conversion_date is not None and "conversion_form" not in str(
-                    conversion_info_source
-                ):
-                    conversion_info_source = (
-                        str(conversion_info_source) + ", conversion_form"
-                    )
-                    converted = True
+                converted = True
 
             with warnings.catch_warnings():
-                warnings.simplefilter(action='ignore', category=FutureWarning)
+                warnings.simplefilter(action="ignore", category=FutureWarning)
                 visit_status_df = pd.concat(
                     [
                         visit_status_df,
@@ -370,9 +241,7 @@ if __name__ == "__main__":
     CONVERTED_COUNT = count_converted(config_file=config_file)
     logger.info(f"Found {CONVERTED_COUNT} converted subjects.")
 
-    converted_status_df = compute_converted(
-        config_file, visit_order=constants.visit_order, debug=False
-    )
+    converted_status_df = compute_converted(config_file)
 
     db.df_to_table(
         config_file=config_file,
